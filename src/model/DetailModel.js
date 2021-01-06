7d63df50592a74ba116a00221bf765436e0d423e
Playneko <playneko@naver.com>
2021-01-07 00:01:53 +0900

    React 블로그
    API URL수정

--- src/model/DetailModel.js
+++ src/model/DetailModel.js
@@ -8,7 +8,7 @@ const DetailModel = (id) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('https://cocoa.akibatv.net/?/api/blog/playneko/detail?id=' + id);
+                const response = await axios.get('https://akibatv.playneko.com/?/api/blog/detail?id=' + id + '&projectid=9a27a65f138f8f6f4991323212ebb408');
                 // console.log(response);
                 setJsonData(response.data.detail);
             } catch(e) {
